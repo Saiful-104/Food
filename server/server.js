509b337d7255001adf6996340aa9a556756f0146
Saiful islam <saiful7745islam@gmail.com>
2026-03-18 02:37:47 +0600

    m

--- server/server.js
+++ server/server.js
@@ -11,12 +11,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:3000",
-      "https://your-frontend-url.vercel.app",
-    ],
+    origin: "*",
     credentials: true,
   }),
 );
