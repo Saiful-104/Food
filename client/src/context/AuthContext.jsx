648aff32f5d2ec962ed489ce27c903a010b35192
Saiful islam <saiful7745islam@gmail.com>
2026-03-18 13:29:23 +0600

    add

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -15,6 +15,7 @@ export const useAuth = () => {
 
 // Get API URL
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+console.log("API_URL:", API_URL);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
